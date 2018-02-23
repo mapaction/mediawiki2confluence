b3c29666fa3f222b45318c5abbcbdabcf4ce0070
Luke Murphy <lukewm@riseup.net>
2018-02-23 16:18:44 +0100

    Nurse labels and spaces into the confluence.
    That's https://trello.com/c/Ykl4HWvO/48-ensure-that-categories-space-labels-works done.

--- m2c/cli.py
+++ m2c/cli.py
@@ -15,38 +15,38 @@ TOP_LEVEL_SPACES = [
     {'general-guidance': {'labels': []}},
     {'standard-procedures': {
         'labels': [
-            'Current_SOPs,_SAPs_and_Security_Manual',
-            'Security Manual',
-            'Non-MapAction Security Advice',
-            'Standard Administrative Procedures',
-            'SAP 01: General Policies',
-            'SAP 02: Human Resources',
-            'SAP 03: Risk Management',
-            'SAP 04: Financial Management',
-            'SAP 05: Fundraising and Marketing',
-            'SAP 05.01: External Communications',
-            'SAP 06: Quality Assurance',
-            'SAP 07: Stakeholder Communications',
-            'SAP 08: Information Technology',
-            'SAP 09: Institutional Partnerships',
-            'SAP 09.01: ECHO',
-            'SAP 09.02: USAID',
-            'SAP 10: Equipment Accounting and Management',
-            'Standard Operational Procedures',
+            'Current-SOPs-SAPs-and-Security-Manual',
+            'Security-Manual',
+            'Non-MapAction Security-Advice',
+            'Standard-Administrative-Procedures',
+            'SAP-01-General-Policies',
+            'SAP-02-Human-Resources',
+            'SAP-03-Risk-Management',
+            'SAP-04-Financial-Management',
+            'SAP-05-Fundraising-and-Marketing',
+            'SAP-05-01-External-Communications',
+            'SAP-06-Quality-Assurance',
+            'SAP-07-Stakeholder-Communications',
+            'SAP-08-Information-Technology',
+            'SAP-09-Institutional-Partnerships',
+            'SAP-09-01-ECHO',
+            'SAP-09-02-USAID',
+            'SAP-10-Equipment-Accounting-and-Management',
+            'Standard-Operational-Procedures',
         ]
     }},
     {'internal-training': {
         'labels': [
-            '2017_Team_Training',
-            '2016_Team_Training',
-            '2015_Team_Training',
-            '2014_Team_Training',
-            '2013_Team_Training',
-            '2012_Team_Training',
-            '2011_Team_Training',
-            'Team_Training_Diary',
-            'External_Training_(Cost_Recoverable)',
-            'Partner_Training',
+            '2017-Team-Training',
+            '2016-Team-Training',
+            '2015-Team-Training',
+            '2014-Team-Training',
+            '2013-Team-Training',
+            '2012-Team-Training',
+            '2011-Team-Training',
+            'Team-Training-Diary',
+            'External-Training-Cost-Recoverable',
+            'Partner-Training',
         ]
     }}
 ]
@@ -108,17 +108,46 @@ def main():
 @click.option('--undo', is_flag=True, help='Undo creation of the spaces')
 @click.option('--verbose', is_flag=True, help='The computer will speak to you')
 def spaces(undo, verbose):
-    """Create the agreed upon top level spaces"""
-    base = get_confluence_cmd()
+    """Create top level spaces"""
+    space_keys = chain(*[k.keys() for k in TOP_LEVEL_SPACES])
 
-    space_cmd = get_action_cmd('addSpace', space='testspace', name='Test Space')
-    if undo:
-        space_cmd = get_action_cmd('removeSpace', space='testspace')
+    for space_key in space_keys:
+        formatted_key = "".join(space_key.split('-'))
+        space_name = " ".join(map(str.capitalize, space_key.split('-')))
 
-    command = base + space_cmd
-    output = run_confluence_cmd(command, verbose=verbose)
-    click.echo(output)
+        action, args = 'addSpace', dict(space=formatted_key, name=space_name)
+        if undo:
+            action, args = 'removeSpace', dict(space=formatted_key)
 
-    # for each key in TOP_LEVEL_SPACES, create a space
-    # for each label inside that key, label that space with them
-    # If we pass --undo, delete everything
+        space_cmd = get_action_cmd(action, **args)
+        base = get_confluence_cmd()
+        command = base + space_cmd
+
+        output = run_confluence_cmd(command, verbose=verbose)
+        click.echo(output)
+
+
+@main.command()
+@click.option('--undo', is_flag=True, help='Undo creation of the labels')
+@click.option('--verbose', is_flag=True, help='The computer will speak to you')
+def labels(undo, verbose):
+    """Create labels on top level spaces"""
+    space_keys = chain(*[k.keys() for k in TOP_LEVEL_SPACES])
+    for key, space_dict in zip(space_keys, TOP_LEVEL_SPACES):
+        labels = space_dict[key]['labels']
+
+        if not labels:
+            continue
+
+        formatted_labels = ','.join(labels)
+        formatted_key = ''.join(key.split('-'))
+
+        action = 'removeLabels' if undo else 'addLabels'
+        args = dict(space=formatted_key, labels=formatted_labels)
+
+        label_cmd = get_action_cmd(action, **args)
+        base = get_confluence_cmd()
+        command = base + label_cmd
+
+        output = run_confluence_cmd(command, verbose=verbose)
+        click.echo(output)
