dd50745382aaeaaff8f11ab9934cd6bb9dc9f4c5
Luke Murphy <lukewm@riseup.net>
2018-03-05 11:14:34 +0100

    Add subprocess, uuid and sort imports.

--- m2c/cli.py
+++ m2c/cli.py
@@ -3,12 +3,12 @@ from itertools import chain
 from os import environ
 from os.path import abspath, dirname
 from pprint import pprint
-from subprocess import CalledProcessError, check_output
-
-from bs4 import BeautifulSoup
+from subprocess import STDOUT, CalledProcessError, check_output
+from uuid import uuid4
 
 import click
 import requests
+from bs4 import BeautifulSoup
 
 import mwclient
 import panflute
